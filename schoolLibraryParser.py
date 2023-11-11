a119e18fcbf39d6342cfe0dd9105d6b5c633c312
Luke Larson <larson.luke@hotmail.com>
2023-11-11 11:55:39 -0700

    First working version of the parser

--- schoolLibraryParser.py
+++ schoolLibraryParser.py
@@ -1,38 +1,97 @@
 import argparse
 import pandas as pd
+import re
 
 
-def getTitle(line):
-    line.strip()
+class book(object):
+    def __init__(self):
+        self.title = ''
+        self.type = ''
+        self.callNumber = ''
+        self.editorDescription = ''
+        self.author = ''
+        self.subLocation = ''
+        self.series = ''
+        self.published = ''
+        self.readingLevel = ''
+        self.interestLevel = ''
+        self.lexicalCategoryID = ''
+        self.copiesAvailable = ''
+
+    def toList(self):
+        return [
+            self.title,
+            self.type,
+            self.callNumber,
+            self.editorDescription,
+            self.author,
+            self.subLocation,
+            self.series,
+            self.published,
+            self.readingLevel,
+            self.interestLevel,
+            self.lexicalCategoryID,
+            self.copiesAvailable
+        ]
+
+
+def getTitle(line, book):
+    line = line.strip()
     if line.endswith("Open"):
         line = line[:-5]
-    return line.strip()
+    book.title = line.strip()
 
 
-def getTypeCallNumAuthor(line):
-    line.strip()
-    data = line.split()
+def getTypeCallNumAuthor(line, book):
+    line = line.strip()
+    data = line.split('  \t')
+    type = ''
+    callNum = ''
+    author = ''
 
-    if data[0] != 'Follett':
-        type = data[0]
-        data = data[1:]
-    else:
-        type = data[0] + " " + data[1]
-        data = data[2:]
+    # Get type
+    type = data[0]
 
-    callNum = None
-    author = None
-
-    if data[0] == 'Call' and data[1] == '#:':
-        callNum = " ".join(data[2:])
-        for item in data[2:-1]:
-            if item.endswith(','):
-                author = " ".join([item, data[data.index(item) + 1].replace(',', '').replace('.', '')])
-                break
+    # Get call number and author
+    if 'call #:' in data[1].lower():
+        callNum = data[1]
+        authorSearch = re.search(r'[A-Za-z]+,\s+[A-Za-z]+\s?([A-Za-z][.]?\s?)*', data[1])
+        if authorSearch is not None:
+            author = authorSearch.group()
     else:  # No call number available, return only the type and author
-        author = " ".join(data)
+        author = data[1]
 
-    return type, callNum, author
+    book.type = type
+    book.callNumber = callNum
+    book.author = author
+
+
+def getOther(line, book):
+    line = line.strip()
+    if line.lower().startswith('sublocation'):
+        book.subLocation = line.split(':')[1].strip()
+    elif line.lower().startswith('series'):
+        series = line.split(':')[1].strip()
+        if series.endswith(','):
+            series = series[:-1]
+        book.series = series
+    elif line.lower().startswith('published'):
+        book.published = line.split()[1].strip()
+    elif line.lower().startswith('reading level') or line.lower().startswith('interest level'):
+        data = line.split('  ')
+        if len(data) > 1:
+            book.readingLevel = data[0].split(':')[1].strip()
+            book.interestLevel = '="{0}"'.format(data[1].split(':')[1].strip())
+        else:
+            if 'reading level' in line.lower().split(':')[0]:
+                book.readingLevel = line.split(':')[1].strip()
+            else:
+                book.interestLevel = '="{0}"'.format(line.split(':')[1].strip())
+    elif line.lower().startswith('lexile'):
+        book.lexicalCategoryID = line.split(':')[1].strip()
+    elif line.lower().endswith('available'):
+        data = line.split()
+        book.copiesAvailable = '="{0}/{1}"'.format(data[0], data[2])
 
 
 def main():
@@ -42,9 +101,11 @@ def main():
         description="Parses data from school library database and formats for easier manipulation"
     )
     cmdParser.add_argument('filepath', help='The full filepath for the book database file to parse')
+    cmdParser.add_argument('savepath', help='The full filepath to the location that the resulting CSV should save to.')
     # Get filepath to parse
     cmdArgs = cmdParser.parse_args()
     FILEPATH = cmdArgs.filepath
+    SAVEPATH = cmdArgs.savepath
 
     # Data
     bookCount = 0
@@ -58,27 +119,19 @@ def main():
                 break
 
             if line.strip().lower() == 'cover image':
-                title = getTitle(file.readline())
-                type, callNum, author = getTypeCallNumAuthor(file.readline())
+                myBook = book()
 
-                booksList.append([
-                    title,
-                    type,
-                    callNum,
-                    '',
-                    author,
-                    '',
-                    '',
-                    '',
-                    '',
-                    '',
-                    '',
-                    ''
-                ])
+                getTitle(file.readline(), myBook)
+                getTypeCallNumAuthor(file.readline(), myBook)
+                while True:
+                    line = file.readline()
+                    if line is None or 'add to this list' in line.lower():
+                        break
+                    getOther(line, myBook)
 
+                booksList.append(myBook.toList())
                 bookCount += 1
 
-    print(booksList)
     print('{0} books parsed...'.format(bookCount))
     booksDf = pd.DataFrame(
         booksList,
@@ -97,7 +150,12 @@ def main():
             'Copies Available'
         ]
     )
-    booksDf.to_csv('.\\testresults\\results.csv')
+
+    try:
+        booksDf.to_csv(SAVEPATH)
+        print('Successfully saved results to {0}'.format(SAVEPATH))
+    except Exception as e:
+        print('Could not write books to CSV...\n{0}'.format(e))
 
 
 if __name__ == "__main__":
